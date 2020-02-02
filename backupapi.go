6a531238ef644fdd9f8b8c79f5fd9e7ae78eac3e
Simon Tucker <simon@brotherlogic@gmail>
2020-02-02 15:12:37 -0800

    overage

--- backupapi.go
+++ backupapi.go
@@ -9,3 +9,23 @@ import (
 func (s *Server) SaveToken(ctx context.Context, req *pb.TokenRequest) (*pb.TokenResponse, error) {
 	return &pb.TokenResponse{}, s.KSclient.Save(ctx, TOKEN, req.GetToken())
 }
+
+// GetStats gets the relevant stats for the system
+func (s *Server) GetStats(ctx context.Context, req *pb.StatsRequest) (*pb.StatsResponse, error) {
+	stats := &pb.StatsResponse{}
+	for _, f := range s.config.Files {
+		found := false
+		for _, stat := range stats.GetStats() {
+			if stat.GetState() == f.GetState() {
+				found = true
+				stat.Count++
+			}
+		}
+
+		if !found {
+			stats.Stats = append(stats.Stats, &pb.Stat{State: f.GetState(), Count: 1})
+		}
+	}
+
+	return stats, nil
+}
--- backupapi_test.go
+++ backupapi_test.go
@@ -14,3 +14,19 @@ func TestSaveToken(t *testing.T) {
 		t.Errorf("Bad token save: %v", err)
 	}
 }
+
+func TestBuildState(t *testing.T) {
+	s := InitTestServer()
+	s.config.Files = append(s.config.Files, &pb.BackupFile{})
+	s.config.Files = append(s.config.Files, &pb.BackupFile{})
+
+	stats, err := s.GetStats(context.Background(), &pb.StatsRequest{})
+
+	if err != nil {
+		t.Errorf("Bad stats")
+	}
+
+	if len(stats.GetStats()) != 1 || stats.GetStats()[0].Count != 2 {
+		t.Errorf("Bad stats: %v", stats)
+	}
+}
